abc34265f89d362618cb0f168746897d99883b61
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2024-03-25 17:11:02 -0300

    update portfolio

--- js/main.js
+++ js/main.js
@@ -62,6 +62,7 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
+    
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
